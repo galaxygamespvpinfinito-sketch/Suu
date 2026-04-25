f80f72f09d2d63bdb95e98c78475010aa14ebf74
galaxygamespvpinfinito-sketch <galaxygamespvpinfinito@gmail.com>
2026-04-25 01:01:24 +0100

    Create start_ubuntu.sh

---
+++ start_ubuntu.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# The path of Ubuntu rootfs
+UBUNTUPATH="/data/local/chroot/ubuntu"
+
+# Fix setuid issue
+busybox mount -o remount,dev,suid /data
+
+busybox mount --bind /dev $UBUNTUPATH/dev
+busybox mount --bind /sys $UBUNTUPATH/sys
+busybox mount --bind /proc $UBUNTUPATH/proc
+busybox mount -t devpts devpts $UBUNTUPATH/dev/pts
+
+# /dev/shm for Electron apps
+mkdir -p $UBUNTUPATH/dev/shm
+busybox mount -t tmpfs -o size=256M tmpfs $UBUNTUPATH/dev/shm
+
+# Mount sdcard
+busybox mount --bind /sdcard $UBUNTUPATH/sdcard
+
+# chroot into Ubuntu
+busybox chroot $UBUNTUPATH /bin/su - root
